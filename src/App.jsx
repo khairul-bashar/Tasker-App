d175f4704ce20a2097e9682fc94ca0e59f84678f
khairul-bashar <khairulbashar5424@gmail.com>
2024-01-18 20:56:23 +0600

    design html template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-export default function App(){
-    return (
-        <p>Welcome to tasker app</p>
-    );
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBord from "./task/TaskBord";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <TaskBord/>
+      <Footer />
+    </>
+  );
 }
